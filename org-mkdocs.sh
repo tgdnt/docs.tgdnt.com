428dacb2d284a6e7e3a7e5b48f9c84b0c5e84362
Tiago Donato <tgdnt@pm.me>
2018-12-03 12:07:20 -0600

    Enables Travis CI
    * Creates .travis.yml
    * Creates org-mkdocs.sh
    * Uses os xenial so I can have access to pandoc 1.16, where
    the --wrap=preserve option works
    * Uses markdown_github, because gfm does not yet work on 1.16

---
+++ org-mkdocs.sh
@@ -0,0 +1,5 @@
+find . -name "*.org" | while read -r filename; do
+    newfilename=$(echo $filename | sed -e "s/\.org/.md/g")
+    pandoc -t markdown_github --wrap=preserve -o $newfilename $filename
+done
+mkdocs build
