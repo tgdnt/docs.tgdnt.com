fcdd8dbc629e408b7a713ffa1dd8a8308cec6f75
Tiago Donato <tgdnt@pm.me>
2018-12-03 19:30:38 -0600

    Implements deployment via Travis CI
    * Adds encrypted AWS credentials to .travis.yml
    * Adds aws commands to org-mkdocs.sh

--- org-mkdocs.sh
+++ org-mkdocs.sh
@@ -3,3 +3,6 @@ find . -name "*.org" | while read -r filename; do
     pandoc -t markdown_github --wrap=preserve -o $newfilename $filename
 done
 mkdocs build
+aws s3 sync site/ s3://docs.tgdnt.com --acl public-read --delete
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id E3G46GREEWAVOG --paths '/*'
