15aed3a13ebff3eda2848b149670edf7494535f6
Tiago Donato <tgdnt@pm.me>
2018-12-04 20:35:40 -0600

    Implements ~./org-mkdocs.sh test~
    * ./org-mkdocs.sh test will run mkdocs serve instead of build
    and exit out of the script before running aws commands.

--- org-mkdocs.sh
+++ org-mkdocs.sh
@@ -2,6 +2,7 @@ find . -name "*.org" | while read -r filename; do
     newfilename=$(echo $filename | sed -e "s/\.org/.md/g")
     pandoc -t markdown_github --wrap=preserve -o $newfilename $filename
 done
+[[ $1 == 'test' ]] && mkdocs serve && exit 0
 mkdocs build
 aws s3 sync site/ s3://docs.tgdnt.com --acl public-read --delete
 aws configure set preview.cloudfront true
